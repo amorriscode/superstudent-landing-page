43915c2e089bda5cbe6069539e9de473f426a386
Anthony Morris <anthony@amorrissound.com>
2020-08-07 18:59:52 -0700

    Fix nav padding

--- components/nav.js
+++ components/nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Nav() {
   return (
     <nav className="bg-ss-blue md:bg-transparent pt-2 md:pt-5">
-      <ul className="flex justify-between items-center container mx-auto p-5 ">
+      <ul className="flex justify-between items-center container mx-auto px-5 sm:px-0 py-5">
         <li>
           <Link href="/">
             <a className="leading-none text-white md:text-ss-blue font-logo text-5xl">
